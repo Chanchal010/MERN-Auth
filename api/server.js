18122778ecb308f224eee9b7126e3b9f3538c73d
Chanchal010 <chanchalbag112@gmail.com>
2024-06-04 22:17:02 +0530

    connect database

---
+++ api/server.js
@@ -0,0 +1,19 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+import connectDb from "./db/connectDb.js";
+
+
+dotenv.config({
+    path: "./.env",
+});
+
+
+connectDb()
+.then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB Connection ERROR!! ", error);
+})
