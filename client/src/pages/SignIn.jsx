b9eca2b5294d3e4c3586de6fb126011fe842c7ab
Chanchal010 <chanchalbag112@gmail.com>
2024-06-08 20:39:07 +0530

    minor spelling mistake

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -72,7 +72,7 @@ export default function SignIn() {
       <div className='flex gap-2 mt-5'>
         <p>Have not an account?</p>
         <Link to='/sign-up'>
-          <span className='text-blue-500'>Sign in</span>
+          <span className='text-blue-500'>Sign Up</span>
         </Link>
       </div>
       <p className='text-red-700 mt-5'>
